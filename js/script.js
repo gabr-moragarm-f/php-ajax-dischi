2fcfac2659c67ff9f2976fd72abad1955811ab77
Gabriel Moraga <agarom97@gmail.com>
2021-03-03 18:46:05 +0100

    aggiunto metodo che ordina dati json (sortJson)

--- js/script.js
+++ js/script.js
@@ -13,38 +13,34 @@ var app = new Vue({
     .then(function(response) {
       self.disks = response.data.disks;
 
-      alert()
-
-      sortJson(self.disks, 'title');
-
-      // function compareStrings(a, b) {
-      //   // Assuming you want case-insensitive comparison
-      //   a = a.toLowerCase();
-      //   b = b.toLowerCase();
-      //
-      //   return (a < b) ? -1 : (a > b) ? 1 : 0;
-      // }
-      //
-      // self.disks.sort(function(a, b) {
-      //   return compareStrings(a.title, b.title);
-      // })
-
       self.genres = response.data.genres;
+
+      self.sortJson(self.disks, 'title');
+
+      self.sortJson(self.genres);
     });
   },
   methods:{
+    prova: function() {
+      alert('prova');
+    },
     sortJson: function(array, key) {
       function compareStrings(a, b) {
-        // Assuming you want case-insensitive comparison
         a = a.toLowerCase();
         b = b.toLowerCase();
 
         return (a < b) ? -1 : (a > b) ? 1 : 0;
       }
 
-      array.sort(function(a, b) {
-        return compareStrings(a[key], b[key]);
-      })
+      if (key) {
+        array.sort(function(a, b) {
+          return compareStrings(a[key], b[key]);
+        })
+      }else {
+        array.sort(function(a, b) {
+          return compareStrings(a, b);
+        })
+      }
     },
     filterGenre: function() {
       let self = this;
@@ -52,9 +48,11 @@ var app = new Vue({
       axios.get('http://localhost//Boolean/Day%2072/php-ajax-dischi/app/server.php?genre=' + this.selectedGenre)
       .then(function(response) {
         self.disks = response.data.disks;
+
+        self.sortJson(self.disks, 'title');
       });
     }
-  }
+  },
 });
 // http://localhost//Boolean/Day%2072/php-ajax-dischi/index.html
 // http://localhost//Boolean/Day%2072/php-ajax-dischi/app/server.php
